df662bb7a0a9c1d6407e1a2f98f24b64f1832457
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-04-30 21:42:05 +0200

    feat: Project 1 - tasks 0, 1 and 2

--- 1_005_Project_Unscramble_Computer_Science_Problems/P0/Task1.py
+++ 1_005_Project_Unscramble_Computer_Science_Problems/P0/Task1.py
@@ -18,3 +18,15 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+if __name__ == '__main__':
+    tel_numbers = []
+    g = globals()
+    for file in ['texts', 'calls']:
+        for column in [0, 1]:
+            temp_numbers = [data[column] for data in g[file]]
+            tel_numbers += temp_numbers
+
+    tel_numbers = set(tel_numbers)
+
+    print("There are {} different telephone numbers in the records.".format(len(tel_numbers)))
