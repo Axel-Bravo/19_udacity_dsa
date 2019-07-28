7d955e2cb86ea544599bd0d260bc4fd1b5f34de2
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-07-28 10:50:07 +0200

    fix & chore:  corrected function call parameters + added required 3 examples of the code

--- 2_006_Project_Show_me_Data_Structures/002_Problem_2.py
+++ 2_006_Project_Show_me_Data_Structures/002_Problem_2.py
@@ -27,13 +27,21 @@ def find_files(suffix, path):
     path_folders = [file for file in path_elements if '.' not in file]
 
     for folder in path_folders:
-        path_files.extend(find_files(suffix='c', path=path + '/' + folder))
+        path_files.extend(find_files(suffix=suffix, path=path + '/' + folder))
 
     return path_files
 
 
-#%% Testing
-
+#%% Testing official
+# Testing preparation
 path_base = os.getcwd() + '/testdir'
+
+# Testing procedure
 print(find_files(suffix='c', path=path_base))
 # ['t1.c', 'a.c', 'a.c', 'b.c']
+
+print(find_files(suffix='h', path=path_base))
+# ['t1.h', 'a.h', 'a.h', 'b.h']
+
+print(find_files(suffix='z', path=path_base))
+# []
