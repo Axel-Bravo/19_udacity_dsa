09ad5ce9b0e7aa5dda1bed2472fb7a6889e15555
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-08-03 13:14:49 +0200

    feat: Project 3- Algorithm vs Problem - Problem 2

---
+++ 3_003_Project_Problems_vs_Algorithms/002_Problem_2.py
@@ -0,0 +1,53 @@
+#%% Imports and functions declarations
+def binary_search_recursive(array: list, target: int, start_index: int, end_index:int) -> int:
+    if start_index > end_index:
+        return -1
+
+    mid_index = (start_index + end_index) // 2
+    mid_element = array[mid_index]
+
+    if mid_element == target:
+        return mid_index
+
+    index_left_side = binary_search_recursive(array, target, start_index, mid_index - 1)
+    index_right_side = binary_search_recursive(array, target, mid_index + 1, end_index)
+
+    return max(index_left_side, index_right_side)
+
+
+def rotated_array_search(input_list: list, number: int) -> int:
+    """
+    Find the index by searching in a rotated sorted array
+
+    Args:
+       input_list(array): Input array to search
+       number (int): target to search
+    Returns:
+       int: Index or -1
+    """
+
+    return binary_search_recursive(array=input_list, target=number, start_index=0, end_index=len(input_list)-1)
+
+
+def linear_search(input_list, number):
+    for index, element in enumerate(input_list):
+        if element == number:
+            return index
+    return -1
+
+
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+#%% Testing Official
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 10])
--- 3_003_Project_Problems_vs_Algorithms/explanation.md
+++ 3_003_Project_Problems_vs_Algorithms/explanation.md
@@ -1,10 +1,26 @@
 # Project Explanation
 Explanation of the different project submodules
 
-## Project 1
+## Problem 1
 The principle of this algorithm is to "think" that the square root of a number has the property of being the number to 
 the power of 2, hence the _floored squared root_ is given __when the floored divion result is the same as the divisor__.
 
 ### Time complexity:
 In this case time complexity is __O(log(n))__, as we transverse all integer number till finding n (being _s_ the 
-number of times this is done); using the formula __s² = n__ , which is equivalent to __log(n)=s__ 
+number of times this is done); using the formula __s² = n__ , which is equivalent to __log(n)=s__.
+
+## Problem 2
+The principle employed in this algorithms is based directly in the _binary search_ algorithms, differently, to this 
+implementations, in its structure, it has been decided to be employed a __more divide approach__, rather than
+computationally expensive on previous levels to _spare_ some division; e.g. when the lists are of size 2, both values
+could have been checked (though this would have increased our __time complexity__). 
+
+### Time complexity
+The time complexity being an algorithm based on binary search is __O(log(n))__.  The number of iterations we perform,
+i.e. recursive depth, follows the rule of _recursive_depth^2 = n_. Thus if we isolate the number of iterations in
+relation to the __input space__ (n), we obtain __log(n) = recursive_depth__.
+
+## Problem 3
+
+
+### Time complexity 
