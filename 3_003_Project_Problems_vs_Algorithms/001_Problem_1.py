f5479236d4c43b0fec77c56c9d3a56255226ca93
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-08-03 12:08:39 +0200

    feat: Project 3 - Algorithm vs Problem - Problem 1

---
+++ 3_003_Project_Problems_vs_Algorithms/001_Problem_1.py
@@ -0,0 +1,31 @@
+#%% Imports and functions declaration
+
+
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+
+    num_div = 1
+
+    while True:
+        if number == 0:
+            return 0
+        elif (number // num_div) == num_div:
+            return num_div
+        else:
+            num_div += 1
+
+
+#%% Testing - Official
+
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
---
+++ 3_003_Project_Problems_vs_Algorithms/explanation.md
@@ -0,0 +1,10 @@
+# Project Explanation
+Explanation of the different project submodules
+
+## Project 1
+The principle of this algorithm is to "think" that the square root of a number has the property of being the number to 
+the power of 2, hence the _floored squared root_ is given __when the floored divion result is the same as the divisor__.
+
+### Time complexity:
+In this case time complexity is __O(log(n))__, as we transverse all integer number till finding n (being _s_ the 
+number of times this is done); using the formula __s² = n__ , which is equivalent to __log(n)=s__ 
