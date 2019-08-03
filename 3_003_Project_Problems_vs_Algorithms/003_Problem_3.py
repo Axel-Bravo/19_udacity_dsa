f8b4c57fd2cd7eb05b382295d76cbf9d8d534753
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-08-03 19:13:32 +0200

    feat: Project 3 - Algorithm vs Problem - Problem 3

---
+++ 3_003_Project_Problems_vs_Algorithms/003_Problem_3.py
@@ -0,0 +1,121 @@
+#%% Imports and functions declarations
+def rearrange_digits(input_list: list, first_layer: bool = False) -> list:
+    """
+        Rearrange Array Elements so as to form two number such that their sum is maximum.
+
+        Args:
+           input_list(list): Input List
+           first_layer(bool): placeholder to know if we are in the first layer of the recursion (special case)
+        Returns:
+           (int),(int): Two maximum sums
+        """
+
+    if len(input_list) <= 1:
+        return input_list
+
+    mid = len(input_list) // 2
+    left = input_list[:mid]
+    right = input_list[mid:]
+
+    left = rearrange_digits(left)
+    right = rearrange_digits(right)
+
+    return merge(left, right, first_layer)
+
+
+def merge(left: list, right: list, first_layer: bool = False) -> list:
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    if first_layer:  # Special case for the last merging step
+        num_max_left = ''
+        num_max_right = ''
+        num_to_left = True
+
+        # Alternating between left and right indexes
+        while left_index < len(left) and right_index < len(right):
+            if left[left_index] > right[right_index]:
+                if num_to_left:
+                    num_max_left = str(right[right_index]) + num_max_left
+                else:
+                    num_max_right = str(right[right_index]) + num_max_right
+                right_index += 1
+            else:
+                if num_to_left:
+                    num_max_left = str(left[left_index]) + num_max_left
+                else:
+                    num_max_right = str(left[left_index]) + num_max_right
+                left_index += 1
+
+            num_to_left = not num_to_left  # Distribute the numbers on each of the list
+
+        # Exhausting remaining index
+        while left_index < len(left):   # left index is not exhausted
+            if num_to_left:
+                num_max_left = str(left[left_index]) + num_max_left
+            else:
+                num_max_right = str(left[left_index]) + num_max_right
+
+            left_index += 1
+            num_to_left = not num_to_left
+
+        while right_index < len(right):  # right index is not exhausted
+            if num_to_left:
+                num_max_left = str(right[right_index]) + num_max_left
+            else:
+                num_max_right = str(right[right_index]) + num_max_right
+
+            right_index += 1
+            num_to_left = not num_to_left
+
+        return [int(num_max_left), int(num_max_right)]
+
+    else:  # Normal merging case
+        while left_index < len(left) and right_index < len(right):
+            if left[left_index] > right[right_index]:
+                merged.append(right[right_index])
+                right_index += 1
+            else:
+                merged.append(left[left_index])
+                left_index += 1
+
+        merged += left[left_index:]
+        merged += right[right_index:]
+
+        return merged
+
+
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+#%% Testing - Official
+print('Test 1:')
+list_num = [1, 2, 3, 4, 5]
+result = rearrange_digits(input_list=list_num, first_layer=True)
+if result == [531, 42]:
+    print('Pass \n')
+else:
+    print("Fail \n")
+
+print('Test 2:')
+list_num = [4, 6, 2, 5, 9, 8]
+result = rearrange_digits(input_list=list_num, first_layer=True)
+if result == [852, 964]:
+    print('Pass \n')
+else:
+    print("Fail \n")
+
+print('Test 3:')
+list_num = [1, 2, 3]
+result = rearrange_digits(input_list=list_num, first_layer=True)
+if result == [31, 2]:
+    print('Pass \n')
+else:
+    print("Fail \n")
--- 3_003_Project_Problems_vs_Algorithms/explanation.md
+++ 3_003_Project_Problems_vs_Algorithms/explanation.md
@@ -21,6 +21,18 @@ i.e. recursive depth, follows the rule of _recursive_depth^2 = n_. Thus if we is
 relation to the __input space__ (n), we obtain __log(n) = recursive_depth__.
 
 ## Problem 3
+This problem, as stated to be solved in _time complexity_ of __O(n*log(n))__, has given the clue to be tackled by a 
+variation of the __merge sort__ algorithm. Indeed, it is a _merge sort_ algorithm, except for the particular treatment 
+if gives to the comparison of results coming from the previous recursion, if we are on the _first level_ of the
+recursion. In this case, it does the _comparison_, as usual, but then starts saving the results on 
+__alternative lists__, which are then returned as the results. 
 
+The usage of this _alternative list saving_ is due to the fact that having the list perfectly sorted, if we start from
+the index[0] and give alternatively a value to each list, occuping this value an increasing digit position, we 
+__always__ obtain a combination that satisfies the condition of having a __maximum sum of two numbers__ and __maximum a
+digit of difference between them__.  
 
 ### Time complexity 
+As the base of the algorithm is the __merge sort__, having a time complexity of __O(n*log(n))__, and there have been no
+substantial modifications to the algorithm; just the addition of __O(1)__ operations, the time complexity remains 
+_equal_.
