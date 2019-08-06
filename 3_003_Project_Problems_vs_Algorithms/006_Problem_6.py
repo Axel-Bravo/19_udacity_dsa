b93e49b8e6c6639aa8ea3311e53517237d17274c
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-08-06 20:53:50 +0200

    feat: Basis Algorithms - Project - Problem 6 -> review corrected

--- 3_003_Project_Problems_vs_Algorithms/006_Problem_6.py
+++ 3_003_Project_Problems_vs_Algorithms/006_Problem_6.py
@@ -8,6 +8,10 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
+
+    if len(ints) == 0:
+        return None
+
     max_val = - float("inf")
     min_val = float("inf")
 
@@ -20,17 +24,31 @@ def get_min_max(ints):
     return (min_val, max_val)
 
 #%% Testing - Official
+# Normal cases
+print('Normal Cases:')
 # Case 1
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
 
 # Case 2
 l = [i for i in range(-12, 25)]  # a list containing -12 - 24
 random.shuffle(l)
-print ("Pass" if ((-12, 24) == get_min_max(l)) else "Fail")
+print("Pass" if ((-12, 24) == get_min_max(l)) else "Fail")
+print('\n')
 
+# Edge cases
+print('Edge Cases:')
 # Case 3
 l = [i for i in range(300, 301)]  # a list containing 300
 random.shuffle(l)
-print ("Pass" if ((300, 300) == get_min_max(l)) else "Fail")
+print("Pass" if ((300, 300) == get_min_max(l)) else "Fail")
+
+# Case 4
+l = []  # an empty list
+print("Pass" if (None == get_min_max(l)) else "Fail")
+
+# Case 5
+l = [i for i in range(-24, -1)]  # a list containing -24 - -2
+random.shuffle(l)
+print("Pass" if ((-24, -2) == get_min_max(l)) else "Fail")
--- 3_003_Project_Problems_vs_Algorithms/explanation.md
+++ 3_003_Project_Problems_vs_Algorithms/explanation.md
@@ -70,8 +70,9 @@ This problem focuses on __finding max and min values__ from an unsorted array, w
 and here _lies the key_, not being required to sort anything, we can solve the problem with a single transversal and
  two placeholders, as reference for _min_ and _max_ values.
 
-### Time complexity
-In this case, we perform a __single transverse__ of the whole input, being the time complexity of __O(n)__.
+### Time and Space complexity
+In this case, we perform a __single transverse__ of the whole input, being the time complexity of __O(n)__. In respect 
+to the _space complexity_, we have just a pair of pointers, hence, it is independent from _input size_; __O(1)__.
 
 ## Problem 7 
 It is similar to __problem 5__, except for the _edge cases_, like "root handler". This problem is focused on the 
